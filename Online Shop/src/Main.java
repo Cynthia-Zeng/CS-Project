468544000bcff36103e256415115cdbb836d9320
Cynthia <348732272@gapps.yrdsb.ca>
2025-01-13 10:55:11 -0500

    Shows 3 categories, products, price, and stock, has out of stock

--- Online Shop/src/Main.java
+++ Online Shop/src/Main.java
@@ -27,16 +27,37 @@ public class Main {
         String[] electronicP = {"Computer", "Phone", "Tablet"};
         String[] clothingP = {"T-shirt", "Sweater", "Shoes"};
         String[] bookP = {"Harry Potter", "Percy Jackson", "Hunger Games"};
-
+        int stock;
         if (action.equals("Electronics")) {
-            for (String items: electronicP)
-                System.out.println(items +
-                        "\n Price: $" + Math.round((Math.random() * 124 + 370.99) * 100) / 100);
-                if ((int)(Math.random() * 9 + 0) == 0)
-                    System.out.println("Stock: Out of Stock");
+            for (String items: electronicP) {
+                System.out.print(items +
+                        "\nPrice: $" + (double)Math.round((Math.random() * 124 + 370.99) * 100) / 100);
+                stock = (int)(Math.random() * 10 + 0);
+                if (stock == 0)
+                    System.out.println("\nStock: Out of Stock");
                 else
-                    System.out.println("\n Stock: " + (int)(Math.random() * 9 + 0));
-            action = sc.nextLine();
+                    System.out.println("\nStock: " + stock);
+            }
+        } else if (action.equals("Clothing")) {
+            for (String items: clothingP) {
+                System.out.print(items +
+                        "\nPrice: $" + (double)Math.round((Math.random() * 100 + 20.99) * 100) / 100);
+                stock = (int)(Math.random() * 10 + 0);
+                if (stock == 0)
+                    System.out.println("\nStock: Out of Stock");
+                else
+                    System.out.println("\nStock: " + stock);
+            }
+        } else if (action.equals("Books")) {
+            for (String items: bookP) {
+                System.out.print(items +
+                        "\nPrice: $" + (double)Math.round((Math.random() * 30 + 20.99) * 100) / 100);
+                stock = (int)(Math.random() * 10 + 0);
+                if (stock == 0)
+                    System.out.println("\nStock: Out of Stock");
+                else
+                    System.out.println("\nStock: " + stock);
+            }
         }
         sc.close();
         }
