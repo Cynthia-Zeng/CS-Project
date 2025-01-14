96da35e9240e0df5b8c1ed193f9b4b42ec5d0f94
Cynthia <348732272@gapps.yrdsb.ca>
2025-01-14 09:52:39 -0500

    Started Purchasing Mechanic

--- Online Shop/src/Main.java
+++ Online Shop/src/Main.java
@@ -11,54 +11,110 @@ public class Main {
             System.out.println("Invalid input, please try again.");
             action = sc.nextLine();
         }
-        // categories
+        // Categories
         String[] category = {"Electronics", "Clothing", "Books"};
-        // let users browse categories
+        // Products, Price, Stock
+        String[] electronicP = {"Computer", "Phone", "Tablet"};
+        double[] ePrice = {279.99, 197.00, 119.99};
+        ArrayList<Integer> eStock = new ArrayList<>();
+        // Stock Random
+        for (int i = 0; i < 3; i++)
+            eStock.add((int)(Math.random() * 10 + 0));
+        String[] clothingP = {"T-shirt", "Sweater", "Shoes"};
+        double[] cPrice = {19.90, 39.90, 130.00};
+        ArrayList<Integer> cStock = new ArrayList<>();
+        // Stock Random
+        for (int i = 0; i < 3; i++)
+            cStock.add((int)(Math.random() * 10 + 0));
+        String[] bookP = {"Harry Potter", "Percy Jackson", "Hunger Games"};
+        double[] bPrice = {11.79, 20.24, 25.89};
+        ArrayList<Integer> bStock = new ArrayList<>();
+        // Stock Random
+        for (int i = 0; i < 3; i++)
+            bStock.add((int)(Math.random() * 10 + 0));
+        // Cart
+        int item;
+        ArrayList<String> cart = new ArrayList<>();
+        ArrayList<Integer> cartPrice = new ArrayList<>();
+        ArrayList<Integer> cartStock = new ArrayList<>();
+        int i = 1;
         System.out.println("In order to see the selection of products, please enter the name of the category");
         for (String categories: category)
-            System.out.println(categories);
+            System.out.println("\""+categories+"\"");
         action = sc.nextLine();
-        // If not correct , make use enter again
+        // If not correct , make user try again
         while (!action.equals("Electronics") && !action.equals("Clothing") && !action.equals("Books")) {
             System.out.println("Invalid input, please try again.");
             action = sc.nextLine();
         }
-
-        String[] electronicP = {"Computer", "Phone", "Tablet"};
-        String[] clothingP = {"T-shirt", "Sweater", "Shoes"};
-        String[] bookP = {"Harry Potter", "Percy Jackson", "Hunger Games"};
-        int stock;
-        if (action.equals("Electronics")) {
-            for (String items: electronicP) {
-                System.out.print(items +
-                        "\nPrice: $" + (double)Math.round((Math.random() * 124 + 370.99) * 100) / 100);
-                stock = (int)(Math.random() * 10 + 0);
-                if (stock == 0)
-                    System.out.println("\nStock: Out of Stock");
-                else
-                    System.out.println("\nStock: " + stock);
+        while (action.equals("Electronics") || action.equals("Clothing") || action.equals("Books") || action.equals("Categories") || action.equals("Checkout")) {
+            switch (action) {
+                case "Electronics":
+                    for (String items: electronicP) {
+                        System.out.print("\""+ i + "\" " + items + "\nPrice: $" + ePrice[i - 1]);
+                        if (eStock.get(i-1) == 0)
+                            System.out.println("\nStock: Out of Stock");
+                        else
+                            System.out.println("\nStock: " + eStock.get(i - 1));
+                        i ++;
+                    }
+                    System.out.println("Would you like to \"Purchase\" or check out other \"Categories\" or head to \"Checkout\": ");
+                    action = sc.nextLine();
+                    while (!action.equals("Purchase") && !action.equals("Categories") && !action.equals("Checkout")) {
+                        System.out.println("Invalid input, please try again.");
+                        action = sc.nextLine();
+                    }
+                    if (action.equals("Purchase")) {
+                        System.out.println("Enter the number of the item you like to buy: ");
+                        item = sc.nextInt();
+                        while (item != 1 && item != 2 && item != 3) {
+                            System.out.println("Invalid input, please try again.");
+                            item = sc.nextInt();
+                        }
+                        System.out.println("Enter the number of the items you like to buy: ");
+                        cart.add(cart.get(item-1));
+                    }
+                    i = 1;
+                    break;
+                case "Clothing":
+                    for (String items: clothingP) {
+                        System.out.print("\""+ i + "\" \"" + items + "\"\nPrice: $" + cPrice);
+                        i ++;
+                        if (cStock.get(i-1) == 0)
+                            System.out.println("\nStock: Out of Stock");
+                        else
+                            System.out.println("\nStock: " + cStock.get(i-1));
+                    }
+                    i = 1;
+                    break;
+                case "Books":
+                    for (String items: bookP) {
+                        System.out.print("\""+ i + "\" \"" + items + "\"\nPrice: $" + bPrice);
+                        i ++;
+                        if (bStock.get(i-1) == 0)
+                            System.out.println("\nStock: Out of Stock");
+                        else
+                            System.out.println("\nStock: " + bStock.get(i-1));
+                    }
+                    i = 1;
+                    break;
+                case "Categories":
+                    System.out.println("In order to see the selection of products, please enter the name of the category");
+                    for (String categories: category)
+                        System.out.println("\""+categories+"\"");
+                    action = sc.nextLine();
+                    // If not correct , make user try again
+                    while (!action.equals("Electronics") && !action.equals("Clothing") && !action.equals("Books")) {
+                        System.out.println("Invalid input, please try again.");
+                        action = sc.nextLine();
+                    }
+                    break;
+                case "Checkout":
+                    break;
             }
-        } else if (action.equals("Clothing")) {
-            for (String items: clothingP) {
-                System.out.print(items +
-                        "\nPrice: $" + (double)Math.round((Math.random() * 100 + 20.99) * 100) / 100);
-                stock = (int)(Math.random() * 10 + 0);
-                if (stock == 0)
-                    System.out.println("\nStock: Out of Stock");
-                else
-                    System.out.println("\nStock: " + stock);
-            }
-        } else if (action.equals("Books")) {
-            for (String items: bookP) {
-                System.out.print(items +
-                        "\nPrice: $" + (double)Math.round((Math.random() * 30 + 20.99) * 100) / 100);
-                stock = (int)(Math.random() * 10 + 0);
-                if (stock == 0)
-                    System.out.println("\nStock: Out of Stock");
-                else
-                    System.out.println("\nStock: " + stock);
-            }
+            if (action.equals("Checkout"))
+                break;
         }
         sc.close();
-        }
     }
+}
